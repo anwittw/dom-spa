a819a0fdb54ff9eca8c154ccfcc9fef3ec365c75
anwittw <git@andrewittwer.de>
2019-06-11 16:32:13 +0100

    All Done

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,34 @@
-function listenForLinksClick() {
-  // TODO
-  let $links // = ...
+
+let $links = document.querySelectorAll('a')
+let $routes = document.querySelectorAll('[data-route]')
+
+
+function initialRender() {
+  listenForLinksClick()
+  displayOneSectionAndHideOthers('/')
 }
 
+
+function listenForLinksClick() { 
+   $links.forEach(($link) => $link.addEventListener('click', (e) => {
+     e.preventDefault()
+     let href = $link.getAttribute('href')
+     console.log('You clicked on the link where href is', href)
+     displayOneSectionAndHideOthers(href)
+   }))
+
+}
 function displayOneSectionAndHideOthers(href) {
-  // TODO
+  $routes.forEach(($route) => {
+  let dataRoute = $route.getAttribute('data-route')
+  if(dataRoute === href) {
+    $route.style.display = 'block'
+    history.pushState({}, null, )
+  } else {
+    $route.style.display = 'none'
+  }
+})
 }
 
-listenForLinksClick()
+initialRender()
+
